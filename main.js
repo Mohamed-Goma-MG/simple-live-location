d974f91b665429b039972d03d2254f7c7c52e281
Mohamed Goma <mohamed.goma.dev@gmail.com>
2026-09-23 10:57:00 +0300

    update: add watchLocation and show it to the page

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const latEle = document.getElementById("longitude")
+const longEle = document.getElementById("latitude")
+
+const latVEle = latEle.getElementsByClassName("value")[0]
+const longVEle = longEle.getElementsByClassName("value")[0]
+
+console.log(latEle, latVEle, longEle, longVEle)
+
+navigator.geolocation.watchPosition(({coords: {latitude, longitude}}) => {
+  latVEle.textContent = latitude
+  longVEle.textContent = longitude
+
+  console.log("location", {latitude, longitude})
+}, ({message}) => {
+  console.error(message) // ! => User denied geolocation prompt
+})
